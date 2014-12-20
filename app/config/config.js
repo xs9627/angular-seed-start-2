4d6a43b4ca232fe9249c4df046d9241f91b02f22
Sonic.S.Xiang <Sonic.S.Xiang@newegg.com>
2014-12-20 08:50:32 +0800

    add config template

---
+++ app/config/config.js
@@ -0,0 +1,11 @@
+angular.module('managementCenter.config',[])
+.constant('servicesConfig',[
+    {
+        name: 'WXMIS007',
+        url: 'http://XXX'
+    },
+    {
+        name: 'WXMIS009',
+        url: ''
+    }
+]);
